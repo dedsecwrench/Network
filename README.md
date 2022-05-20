e3c02cf0aaf71a3cfd9f8408331e613373e6fd63
Nameera Pathan <66337671+dedsecwrench@users.noreply.github.com>
2022-05-20 20:21:19 +0530

    Network

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
+# CS50's Web Programming with Python and JavaScript
+
+# Project 4 - Network
+
+it's a Django project called project4 that contains a single app called Network.
+
+Screencast - https://youtu.be/MQHf6Qcxtao
+
+# Files in project :
+
+# manage.py
+
+Application to run server
+
 # Network
+
+# project4
+
---
+++ network/static/network/profile.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let unfollowBtn = document.querySelector('#unfollowBtn')
+    if (unfollowBtn !== null) {
+        unfollowBtn.onmouseover = function () {
+            unfollowBtn.value = "Unfollow"
+            unfollowBtn.className = "btn following-btn"
+            unfollowBtn.style.backgroundColor = "#c90d00"
+        }
+        unfollowBtn.onmouseout = function () {
+            unfollowBtn.value = "Following"
+            unfollowBtn.className = "btn following-btn"
+            unfollowBtn.style.backgroundColor = "rgba(29,161,242,1.00)"
+        }
+    }
+});
